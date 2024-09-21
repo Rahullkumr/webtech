f9871cdc1ed3c3ef8187090b8c803ef76d9d97e7
Rahullkumr <kumarrahul771996@gmail.com>
2024-09-22 02:13:52 +0530

    chore: add(JS day19 notes)

--- 03_javascript/day19/day19.js
+++ 03_javascript/day19/day19.js
@@ -1,3 +1,20 @@
 // 20 - Sept - 2024
 
-console.log('give me code someone');
+
+// es6 feature
+    // always try to shorten the syntax
+    // arr.forEach((index,element,wholeArray)=>{})
+
+// arr.forEach(element => console.log(index));
+// arr.forEach(element => console.log(element));
+// arr.forEach(element => console.log(wholeArray));
+
+
+
+
+// example:
+
+test = [1,2,3,4,5]
+test.forEach((index,element,wholeArray)=>{
+    console.log(wholeArray);
+})
